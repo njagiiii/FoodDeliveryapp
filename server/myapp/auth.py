3b50a7ccd7abbf3ff1b9c9eebf438c8d18f067c2
njagiiii <grace.makena@student.moringaschool.com>
2023-10-16 18:56:29 -0400

    moved app into root directory

--- server/myapp/auth.py
+++ server/myapp/auth.py
@@ -1,5 +1,5 @@
 from flask import request,jsonify,Blueprint,flash
-from myapp import bcrypt
+from server.myapp import bcrypt
 from myapp.models import User,TokenBlocklist
 from flask_jwt_extended import (create_access_token,create_refresh_token,
                                 jwt_required,
