26a26038790de11bbc7975b09aee529d35a67376
njagiiii <grace.makena@student.moringaschool.com>
2023-10-16 19:07:01 -0400

    ignored my env

--- server/myapp/user_account.py
+++ server/myapp/user_account.py
@@ -1,5 +1,5 @@
 from flask import jsonify,request,Blueprint
-from myapp.models import User,Order
+from server.myapp.models import User,Order
 from flask_jwt_extended import jwt_required,get_jwt_identity
 from flask_cors import cross_origin
 
